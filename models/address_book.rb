73c309eb51aa9aea24b1d55c93dc3b6e5691f1cd
Alex Gould <alexgould17@gmail.com>
2017-02-12 17:47:07 -0800

    Added remove_entry method to AddressBook & associated test.

--- models/address_book.rb
+++ models/address_book.rb
@@ -15,4 +15,15 @@ class AddressBook
     end
     entries.insert(index, Entry.new(name, phone_number, email))
   end
+
+  def remove_entry(name, phone_number, email)
+    index = -1
+    entries.length.times do |i|
+      if name == entries[i].name && phone_number == entries[i].phone_number && email == entries[i].email
+        index = i
+        break
+      end
+    end
+    entries.delete_at(index) if index != -1
+  end
 end
